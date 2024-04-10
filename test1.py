e521ff8da18c19bc78e550e0e0e21ac68e957018
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-10 15:39:58 +0200

    first commit

---
+++ test1.py
@@ -0,0 +1,26 @@
+"""
+First example with Tkinter.
+We create a simple window which welcomes
+the user
+"""
+
+# We import Tkinter
+from tkinter import *
+
+# We create a window, root of our interface
+window = Tk()
+
+# We create a label (line of text) welcoming
+# Note: the first parameter passed to the Label constructor is our root interface
+field_label = Label(window, text="Welcome to FormationKilo !")
+
+# We display the label in the window
+field_label.pack()
+
+# We start the Tkinter loop which is interrupted when we close the window
+window.mainloop()
+
+
+
+
+
